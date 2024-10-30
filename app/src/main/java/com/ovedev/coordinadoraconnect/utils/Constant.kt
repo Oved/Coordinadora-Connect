27757e544d9f68c69c75ffaf5b46118bb0eacb62
Oved <ovedrincon29@gmail.com>
2024-10-29 19:22:34 -0500

    Constant class is created

---
+++ app/src/main/java/com/ovedev/coordinadoraconnect/utils/Constant.kt
@@ -0,0 +1,11 @@
+package com.ovedev.coordinadoraconnect.utils
+
+class Constant {
+
+    companion object {
+        //URL'S
+        const val URL_VALIDATE_USER = "https://noderedtest.coordinadora.com/api/v1/validacion-usuario/"
+        const val URL_GET_IMAGE_PDF = "https://noderedtest.coordinadora.com/api/v1/obtenerimagen/"
+    }
+
+}
